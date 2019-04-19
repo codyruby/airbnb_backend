20b45cd7c8bffba5405a65a95e111d9cc0cd88f2
Yousri <laifayousri@gmail.com>
2019-04-19 13:57:11 +0200

    Création du model User

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    validates :email,
+    presence: true,
+    uniqueness: true,
+    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
+
+    has_many :reservations
+    has_many :accommodations
+end
